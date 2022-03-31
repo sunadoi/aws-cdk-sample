3018f13ec91d556593aff4b3af9e5db7cc3096b7
sunadoi <y.sunadoi@gmail.com>
2022-03-31 21:23:55 +0900

    lambda

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -1,8 +1,15 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import { Function, Runtime, Code } from "aws-cdk-lib/aws-lambda";
 
 export class CdkSampleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset("lambda"),
+      handler: "hello.handler",
+    });
   }
 }
